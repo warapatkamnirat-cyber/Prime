f9cb4ca477953f9a20b3d369d9b92bb66a1fa2b6
warapatkamnirat-cyber <warapatkamnirat@gmail.com>
2026-04-08 21:42:29 +0700

    Delete auth.js

--- auth.js
+++
@@ -1,9 +0,0 @@
-// เอฟเฟกต์คลิกปุ่ม
-document.querySelectorAll(".btn").forEach(btn => {
-  btn.addEventListener("click", () => {
-    btn.style.transform = "scale(0.9)";
-    setTimeout(() => {
-      btn.style.transform = "scale(1)";
-    }, 150);
-  });
-});
